4bad89fd5463e6dcd50c2d2871dd3c3fab48ae88
이해주 <hj_lee_com@naver.com>
2022-07-31 10:43:25 +0900

    feautre : size config 환경 설정

--- lib/main.dart
+++ lib/main.dart
@@ -7,50 +7,50 @@ import 'package:movie_curation/ui/screens/root/root_paged_view.dart';
 import 'utilities/index.dart';
 
 // Device Preview 셋업 (반응형 레이아웃 확인)
-void main() async {
-  // FRouter.setupRouter();
-  WidgetsFlutterBinding.ensureInitialized(); // Firebase 셋팅
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const MyApp(),
-    ),
-  );
-}
-
 // void main() async {
 //   // FRouter.setupRouter();
 //   WidgetsFlutterBinding.ensureInitialized(); // Firebase 셋팅
 //   await Firebase.initializeApp(
 //     options: DefaultFirebaseOptions.currentPlatform,
 //   );
-//   runApp(const MyApp());
+//   runApp(
+//     DevicePreview(
+//       enabled: !kReleaseMode,
+//       builder: (context) => const MyApp(),
+//     ),
+//   );
 // }
+
+void main() async {
+  // FRouter.setupRouter();
+  WidgetsFlutterBinding.ensureInitialized(); // Firebase 셋팅
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Sizer(
-      builder: (context, orientation, deviceType) {
-        SizeConfig().init(context);
-        return GetMaterialApp(
-          title: 'New Project',
-          debugShowCheckedModeBanner: false,
-          useInheritedMediaQuery: true,
-          initialBinding: AppBinding(),
-          locale: DevicePreview.locale(context),
-          builder: DevicePreview.appBuilder,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: RootPagedView(),
-        );
-      },
-    );
+    return Sizer(builder: (context, orientation, deviceType) {
+      return GetMaterialApp(
+        title: 'New Project',
+        debugShowCheckedModeBanner: false,
+        useInheritedMediaQuery: true,
+        initialBinding: AppBinding(),
+        locale: DevicePreview.locale(context),
+        builder: (context, child) {
+          SizeConfig().init(context);
+          return DevicePreview.appBuilder(context, child);
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: RootPagedView(),
+      );
+    });
   }
 }
