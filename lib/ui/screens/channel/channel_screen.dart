17854b65d4b2f4a6386942b0afd84df64ad07e66
이해주 <hj_lee_com@naver.com>
2022-09-10 15:40:50 +0900

    feature : channelScreen > UI 배치 완료

--- lib/ui/screens/channel/channel_screen.dart
+++ lib/ui/screens/channel/channel_screen.dart
@@ -1,4 +1,5 @@
 import 'package:movie_curation/ui/screens/channel/channel_screen_scaffold.dart';
+import 'package:movie_curation/ui/screens/channel/localWidget/ContentInfoContainerNew.dart';
 import 'package:movie_curation/ui/screens/channel/localWidget/channel_main_info_container.dart';
 import 'package:movie_curation/ui/widgets/new_gradient_button.dart';
 import 'package:movie_curation/ui/widgets/round_cached_img_container.dart';
@@ -16,6 +17,7 @@ class ChannelScreen extends BaseScreen<HomeViewModel> {
 
   @override
   Widget buildScreen(BuildContext context) {
+    final sectionWidth = SizeConfig().screenWidth * 0.6;
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
     return ChannelScreenScaffold(
@@ -38,64 +40,164 @@ class ChannelScreen extends BaseScreen<HomeViewModel> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           /** Top Section **/
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              /* Channel Main Info */
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  const ChannelMainInfoContainer(
-                                      channelImgSize: 120),
-                                  Container(
-                                    margin: const EdgeInsets.only(right: 40),
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          padding: EdgeInsets.zero,
-                                          primary: Colors.transparent),
-                                      onPressed: () {
-                                        _scaffoldKey.currentState!
-                                            .openEndDrawer();
-                                      },
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(10),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: <Widget>[
-                                            Text(
-                                              "더 많은 채널 보기",
-                                              style: AppTextStyle.headline1
-                                                  .copyWith(
-                                                      color: AppColor.textGrey),
+                          Expanded(
+                            flex: 4,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                /* Channel Main Info */
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        const ChannelMainInfoContainer(
+                                            channelImgSize: 120),
+                                        Container(
+                                          margin:
+                                              const EdgeInsets.only(right: 40),
+                                          child: ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                                padding: EdgeInsets.zero,
+                                                primary: Colors.transparent),
+                                            onPressed: () {
+                                              _scaffoldKey.currentState!
+                                                  .openEndDrawer();
+                                            },
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(10),
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: <Widget>[
+                                                  Text(
+                                                    "더 많은 채널 보기",
+                                                    style: AppTextStyle
+                                                        .headline1
+                                                        .copyWith(
+                                                            color: AppColor
+                                                                .textGrey),
+                                                  ),
+                                                  SvgPicture.asset(
+                                                    "assets/icons/right arrow_ic.svg",
+                                                    height: 32,
+                                                    color: AppColor.yellow
+                                                        .withOpacity(0.8),
+                                                  ),
+                                                ],
+                                              ),
                                             ),
-                                            SvgPicture.asset(
-                                              "assets/icons/right arrow_ic.svg",
-                                              height: 40,
-                                              color: AppColor.yellow
-                                                  .withOpacity(0.8),
-                                            ),
-                                          ],
-                                        ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    AppSpace.size40,
+                                    /* Channel Description */
+                                    SizedBox(
+                                      width: sectionWidth,
+                                      child: Text(
+                                        "Lego Star Wars - Stormtrooper in desert (Stop motion shortfilm) #Shorts, Lego Star Wars - Stormtrooper in desert (Stop motion shortfilm) #Shorts, Lego Star Wars - Stormtrooper in desert (Stop motion shortfilm) #Shorts",
+                                        style: AppTextStyle.headline3
+                                            .copyWith(color: Colors.white),
                                       ),
                                     ),
-                                  )
-                                ],
-                              ),
-                              AppSpace.size40,
-                              /* Channel Description */
-                              SizedBox(
-                                width: SizeConfig().screenWidth * 0.6,
-                                child: Text(
-                                  "Lego Star Wars - Stormtrooper in desert (Stop motion shortfilm) #Shorts, Lego Star Wars - Stormtrooper in desert (Stop motion shortfilm) #Shorts, Lego Star Wars - Stormtrooper in desert (Stop motion shortfilm) #Shorts",
-                                  style: AppTextStyle.headline3
-                                      .copyWith(color: Colors.white),
+                                    /* Content Info Section */
+                                  ],
                                 ),
-                              ),
-                            ],
+                                const Spacer(),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "탑건 메버릭",
+                                      style: AppTextStyle.headline1
+                                          .copyWith(color: Colors.white),
+                                    ),
+                                    const SizedBox(
+                                      height: 20,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Container(
+                                          padding: const EdgeInsets.all(6),
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(4),
+                                            color: AppColor.lightGrey,
+                                          ),
+                                          child: Text(
+                                            vm.selectedContent!.adult ?? false
+                                                ? "청소년 관람 불가"
+                                                : "일반",
+                                            style: FontStyles(0, false).gRated,
+                                          ),
+                                        ),
+                                        const SizedBox(width: 12),
+                                        Text(
+                                          vm.selectedContent!.releaseDate !=
+                                                  null
+                                              ? Regex.dateYM(vm.selectedContent!
+                                                  .releaseDate!)
+                                              : "개봉일 확인 불가",
+                                          style: FontStyles(0, false).releaseY,
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      height: 30,
+                                    ),
+                                    SizedBox(
+                                      width: sectionWidth,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                              primary: AppColor.yellow,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 8),
+                                            ),
+                                            onPressed: routeAction,
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                SvgPicture.asset(
+                                                    "assets/icons/play_ic.svg",
+                                                    height: 20,
+                                                    color: Colors.black),
+                                                const SizedBox(width: 6),
+                                                Padding(
+                                                  padding:
+                                                      const EdgeInsets.only(
+                                                          top: 2),
+                                                  child: Text(
+                                                    "인기 컨텐츠",
+                                                    style: FontStyles(0, false)
+                                                        .watchButton,
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                          GradientButton(
+                                              content: '예고편',
+                                              onBtnTapHandler: () {}),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                const Spacer(),
+                              ],
+                            ),
                           ),
-
+                          /** Top Section **/
                           /* Content Post Slider  */
                           const ContentPosterSlider(),
                           /* Category Btn */
--- lib/ui/widgets/content_poster_slider.dart
+++ lib/ui/widgets/content_poster_slider.dart
@@ -7,53 +7,56 @@ class ContentPosterSlider extends BaseView<HomeViewModel> {
   Widget buildView(BuildContext context) {
     return Obx(
       () => Expanded(
-        flex: 8,
-        child: ScrollablePositionedList.builder(
-          padding: EdgeInsets.only(top: contentSliderDivideP),
-          itemCount: vm.selectedContentList?.length ?? 0,
-          initialScrollIndex: 0,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return ValueListenableBuilder<Iterable<ItemPosition>>(
-              valueListenable: vm.itemPositionListener.itemPositions,
-              builder: (context, positions, child) {
-                return GestureDetector(
-                  onTap: () => vm.onPosterItemTapped(index),
-                  child: Container(
-                    margin: const EdgeInsets.only(
-                      right: 32,
-                    ),
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(12)),
-                    child: AspectRatio(
-                      aspectRatio: 39 / 58,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: CachedNetworkImage(
-                          imageUrl:
-                              "https://image.tmdb.org/t/p/w500${vm.selectedContentList![index].posterUrl}",
-                          imageBuilder: (context, imageProvider) => Container(
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: imageProvider,
-                                fit: BoxFit.cover,
+        flex: 3,
+        child: Container(
+          color: Colors.grey,
+          child: ScrollablePositionedList.builder(
+            shrinkWrap: true,
+            itemCount: vm.selectedContentList?.length ?? 0,
+            initialScrollIndex: 0,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return ValueListenableBuilder<Iterable<ItemPosition>>(
+                valueListenable: vm.itemPositionListener.itemPositions,
+                builder: (context, positions, child) {
+                  return GestureDetector(
+                    onTap: () => vm.onPosterItemTapped(index),
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        right: 32,
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12)),
+                      child: AspectRatio(
+                        aspectRatio: 39 / 58,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                "https://image.tmdb.org/t/p/w500${vm.selectedContentList![index].posterUrl}",
+                            imageBuilder: (context, imageProvider) => Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: imageProvider,
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
+                            placeholder: (context, url) => const Center(
+                                child: CircularProgressIndicator()),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
                           ),
-                          placeholder: (context, url) =>
-                              const Center(child: CircularProgressIndicator()),
-                          errorWidget: (context, url, error) =>
-                              const Icon(Icons.error),
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
-            );
-          },
-          itemScrollController: vm.itemScrollController,
-          itemPositionsListener: vm.itemPositionListener,
+                  );
+                },
+              );
+            },
+            itemScrollController: vm.itemScrollController,
+            itemPositionsListener: vm.itemPositionListener,
+          ),
         ),
       ),
     );
